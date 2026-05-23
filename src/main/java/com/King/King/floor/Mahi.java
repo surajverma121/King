8f0b9351801a5cbec33a9eba3705549c5d407cbc
surajverma121 <Surajverma7049214132@gmail.com>
2026-05-23 23:24:46 +0530

    master changes dareckt

--- src/main/java/com/King/King/floor/Mahi.java
+++ src/main/java/com/King/King/floor/Mahi.java
@@ -4,7 +4,15 @@ public class Mahi {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        int c = 33 ;
+
+
+        System.out.println(a+b+c);
+
+        for (int i = 1 ; i<=10 ; i++){
+            // 1 * 5 = 5
+            System.out.println( i + " * " + " 10 " + " = " + i*5 );
+        }
 
     }
 }
--- src/main/java/com/King/King/floor/Suraj.java
+++ src/main/java/com/King/King/floor/Suraj.java
@@ -3,4 +3,6 @@ package com.King.King.floor;
 public class Suraj {
 //     / fghjkl;
 // gggg
+
+    // dandu   confilit dur kar
 }
